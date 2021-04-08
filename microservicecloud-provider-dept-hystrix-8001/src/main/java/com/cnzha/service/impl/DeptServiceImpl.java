f73063d81551d1aadb273797fa99865e9aaacbf2
jsnejcopbe <njv9390xutk620221@126.com>
2021-04-08 17:04:48 +0800

    hystrix和feign

---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/cnzha/service/impl/DeptServiceImpl.java
@@ -0,0 +1,35 @@
+package com.cnzha.service.impl;
+
+import com.cnzha.dao.DeptDao;
+import com.cnzha.entities.Dept;
+import com.cnzha.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService
+{
+	@Autowired
+	private DeptDao dao;
+	
+	@Override
+	public boolean add(Dept dept)
+	{
+		return dao.addDept(dept);
+	}
+
+	@Override
+	public Dept get(Long id)
+	{
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Dept> list()
+	{
+		return dao.findAll();
+	}
+
+}
